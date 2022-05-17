fdc1607a420d9010ee3fdd371b2bdf41b3045d5f
Alfonso Domenici <alfonso.domenici@gmail.com>
2022-05-17 17:02:26 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 console.log("script start");
 console.log(new Date());
+
+let msg = 'ciao xx';
+
+const pi = 3.14;
+
+if(msg === 'ciao'){
+    console.log("il messaggio contiene ciao")
+}else{
+    console.log("il messaggio non contiene ciao")
+}
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+let i=0;
+while(i<10){
+    console.log('--' + i);
+    i+=1;
+}
+
+console.log('somma: ' + somma(10,10));
+
+function somma(n1,n2){
+    let res = n1 + n2;
+    return res;
+}
